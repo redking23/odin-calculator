81846aff7ac07336c78f0354e1334a7662cb72c4
Cantuğ Şahiner <cantugsahiner@gmail.com>
2025-12-27 11:42:48 +1100

    update 1

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
 
 let displayValue = "0";
 
@@ -6,3 +7,20 @@ function updateDisplay() {
   display.value = displayValue;
 }
 updateDisplay();
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.dataset.type === "number") {
+      if (displayValue === "0") {
+        displayValue = button.textContent;
+      } else {
+        displayValue += button.textContent;
+      }
+      updateDisplay();
+    }
+    if (button.textContent === "C") {
+      displayValue = "0";
+      updateDisplay();
+    }
+  });
+});
